9ff82bf45e95be3494c4f67ac541763f4da49b0b
Andrew Duberstein <ajduberstein@gmail.com>
2018-10-28 23:13:46 -0700

    Trying to get these circles to display

--- vis.js
+++ vis.js
@@ -11,75 +11,89 @@ var width = 960,
 var projection = d3.geoAlbersUsa()
 var path = d3.geoPath(projection)
 
-let nodes = null;
+var msaData;
+var displayData;
+
 const svg = d3.select("body").append("svg")
 				.attr("width", width)
 				.attr("height", height);
 
-// This file name is a hack to get around the fact that
-// parcel bundles JSON files, rather than treating them as static
-// assets.
-// See https://github.com/parcel-bundler/parcel/issues/501
-const first = new Promise((res, rej) => {
-  d3.json(require("./us.jsonx"), (error, topo) => {
-    if (error) {
-      console.error(error)
-    }
-    let states = topojson.feature(topo, topo.objects.states)
-  
-  	svg.selectAll("path")
-        .data(states.features).enter()
-        .append("path")
-        .attr("class", "feature")
-        .style("fill", "steelblue")
-        .attr("d", path);
-  
-    svg.append("path")
-      .datum(topojson.mesh(topo, topo.objects.states, (a, b) => { return a !== b }))
-      .attr("class", "mesh")
-      .attr("d", path);
-  
-    d3.csv(require('./input.csv'))
-      .row((d) => {
-        return {
-          msa: d.msa,
-          pt: [+d.lng, +d.lat],
-          x: +d.lng,
-          y: +d.lat,
-          area: +d.area,
-          year: +d.year.substring(0, 4),
-          pct: +d.pct_of_total
-        }}).get((err, rows) => {
-          injectData(rows, d => d.year === 2001, 10)
-          res(rows)
-        })
-  })
+// const loadStates = (res, rej) => {
+//   // This file name is a hack to get around the fact that
+//   // parcel bundles JSON files, rather than treating them as static
+//   // assets.
+//   // See https://github.com/parcel-bundler/parcel/issues/501
+//   d3.json(require("./us.jsonx"), (error, topo) => {
+// 
+//     if (error) {
+//       console.error(error)
+//       rej()
+//     }
+//     let states = topojson.feature(topo, topo.objects.states)
+// 
+//     svg.selectAll("path")
+//         .data(states.features).enter()
+//         .append("path")
+//         .attr("class", "feature")
+//         .style("fill", "steelblue")
+//         .attr("d", path);
+// 
+//     svg.append("path")
+//       .datum(topojson.mesh(topo, topo.objects.states, (a, b) => { return a !== b }))
+//       .attr("class", "mesh")
+//       .attr("d", path);
+//     res() 
+//   })
+// }
+
+const loadMsa = new Promise((res, rej) => {
+  d3.csv(require('./input.csv'))
+    .row((d) => {
+      return {
+        msa: d.msa,
+        pt: [+d.lng, +d.lat],
+        x: +d.lng,
+        y: +d.lat,
+        area: +d.area,
+        year: +d.year.substring(0, 4),
+        pct: +d.pct_of_total
+      }}).get((err, rows) => {
+        if (err) {
+          console.error(err);
+        }
+        res(rows)
+      });
 })
-// d => Math.sqrt(Math.round(d['pct'] * 10000))
-const second = (year, data) => {
-  console.table(data.slice(0, 10))
-  injectData(data.filter(x => x.year == year))
+
+const scale = pct => Math.sqrt(Math.round(pct * 10000))
+
+const updateCircles = (data) => {
+  if (!data) {
+    console.error("Data not initialized")
+  }
+  const locations = svg.selectAll("circle").data(data);
+  locations.enter().append("circle").attr("cx", d => projection(d.pt)[0] )
+    .attr("cy", d => projection(d.pt)[1] )
+	  .attr("fill", "crimson");
 }
 
 var t = d3.transition()
     .duration(100)
     .ease(d3.easeLinear);
 
-const injectData = (data, filterFunc, radiusFunc) => {
-  // add circles to svg
-  nodes = svg.selectAll("circle")
-	.data(data).enter()
-  .filter(filterFunc)
-	.append("circle")
-  .attr("cx", d => { return projection(d.pt)[0] })
-  .attr("cy", d => { return projection(d.pt)[1] })
-  .attr("r", radiusFunc)
-	.attr("fill", "crimson");
-
+let year = 2001
+const tick = () => {
+  year = year < 2018 ? ++year : 2002;
+  const yearData = msaData.filter(d => d.year === year);
+  updateCircles(yearData);
+  console.log(year);
 }
 
 // main
-Promise.resolve(first).then(
-  data => second(2001, data)
-)
-// var simulation = d3.forceSimulation(nodes.nodes())
+loadMsa.then(
+  (data) => {
+    // Turn MSA map into force layout
+    // Size elements by pct of GDP
+    msaData = data
+    let clock = setInterval(tick, 1000);
+})
