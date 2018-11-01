1dbd73e636cb1fb1796ff7d002813bb4f0b0275a
Andrew Duberstein <ajduberstein@gmail.com>
2018-11-01 10:12:15 -0700

    Lint JS

--- vis.js
+++ vis.js
@@ -5,23 +5,22 @@ import * as topojson from 'topojson-client'
 // Dorling carto: https://bl.ocks.org/veltman/938ea2d0ef98c02633bec15d6fb3a177
 // Municipalities of MX: https://bl.ocks.org/mbostock/c4c27dc3724e1ad3d680a581079b2e9c
 
-var width = 960,
-    height = 650;
+let width = 960,
+    height = 550;
 
-var projection = d3.geoAlbersUsa()
-var path = d3.geoPath(projection)
+let projection = d3.geoAlbersUsa();
+let path = d3.geoPath(projection);
 
-var msaData;
-var displayData;
+let msaData;
+let displayData;
 
 const svg = d3.select("body").append("svg")
-				.attr("width", width)
-				.attr("height", height);
+  .attr("width", width)
+  .attr("height", height);
+const header = d3.select("body").append("h1");
+let locations;
 
 // const loadStates = (res, rej) => {
-//   // This file name is a hack to get around the fact that
-//   // parcel bundles JSON files, rather than treating them as static
-//   // assets.
 //   // See https://github.com/parcel-bundler/parcel/issues/501
 //   d3.json(require("./us.jsonx"), (error, topo) => {
 // 
@@ -57,43 +56,40 @@ const loadMsa = new Promise((res, rej) => {
         area: +d.area,
         year: +d.year.substring(0, 4),
         pct: +d.pct_of_total
-      }}).get((err, rows) => {
+      }})
+    .get((err, rows) => {
         if (err) {
           console.error(err);
         }
         res(rows)
       });
-})
+});
 
-const scale = pct => Math.sqrt(Math.round(pct * 10000))
-
-const updateCircles = (data) => {
-  if (!data) {
-    console.error("Data not initialized")
-  }
-  const locations = svg.selectAll("circle").data(data);
-  locations.enter().append("circle").attr("cx", d => projection(d.pt)[0] )
-    .attr("cy", d => projection(d.pt)[1] )
-	  .attr("fill", "crimson");
-}
+const scale = pct => Math.max(Math.pow(pct*100, 2), 2)
 
 var t = d3.transition()
-    .duration(100)
-    .ease(d3.easeLinear);
+          .duration(100)
+          .ease(d3.easeLinear);
 
-let year = 2001
+let year = 2001;
+
 const tick = () => {
-  year = year < 2018 ? ++year : 2002;
-  const yearData = msaData.filter(d => d.year === year);
-  updateCircles(yearData);
-  console.log(year);
+  year = year < 2018 ? year + 1 : 2002;
+  locations.append("circle")
+    .attr("cx", d => projection(d.pt)[0] )
+    .attr("cy", d => projection(d.pt)[1] )
+    .style("fill", "crimson")
+    .transition(t)
+    .duration(500)
+    .style("fill-opacity", d => d.year === year ? 1 : 0)
+    .attr("r", d => scale(d.pct))
+    .text(d => d.pct);
+  header.text(year);
 }
 
-// main
 loadMsa.then(
   (data) => {
-    // Turn MSA map into force layout
-    // Size elements by pct of GDP
-    msaData = data
-    let clock = setInterval(tick, 1000);
+    msaData = data;
+    locations = svg.selectAll('circle').data(msaData).enter();
+    let clock = setInterval(tick, 500);
 })
