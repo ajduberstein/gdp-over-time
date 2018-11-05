193cbd138282985afe2e0502b9d19c00c91d272f
Andrew Duberstein <ajduberstein@gmail.com>
2018-11-04 21:00:54 -0800

    Update parcel build

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+echo "Building"
 parcel build index.html
+echo "Going to gh-pages branch"
 git checkout gh-pages
-mv dist/* .
-rmdir dist
+mv dist/index.html .
 git push
 git checkout -
+echo "Done"
