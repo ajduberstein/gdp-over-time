cf1148474c2a7a72ad444968b328ad46ee2d5692
Andrew Duberstein <ajduberstein@gmail.com>
2018-11-04 20:53:02 -0800

    Add deployment

--- make_tiles/join.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-msa = pd.read_csv('./msa_centers.csv')
-gdp = pd.read_csv('../data/input_no_geo.csv')
-
-result = msa.merge(gdp)
-result.to_csv('../input.csv', index=False)
--- make_tiles/pull_shape.sh
+++
@@ -1,3 +0,0 @@
-wget http://www2.census.gov/geo/tiger/TIGER2017/CBSA/tl_2017_us_cbsa.zip
-wget https://bl.ocks.org/rveciana/raw/ee2119324e835e1bad42d0e4c1b9ab0d/2b2cbd9f7a56e821b36ada304d48934af3ebc27d/us.json
-mv us.json ../us.jsonx
