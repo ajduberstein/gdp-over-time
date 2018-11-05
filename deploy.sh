d5d1c94ffb75aa9c4a4de4ffdd9ff6adf6de1971
Andrew Duberstein <ajduberstein@gmail.com>
2018-11-04 21:17:17 -0800

    Change deploy to support public url

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,17 @@
 set -e
 
 echo "Building"
-parcel build index.html
+parcel build --public-url https://duberste.in/gdp-over-time/ index.html
 echo "Going to gh-pages branch"
 git checkout gh-pages
-mv dist/index.html .
+rm -f *.js *.css *.html
+mv dist/* .
+rmdir dist
+mv us.*.{jsonx,json}
+sed -i '' 's/.jsonx/.json/g' vis.*.js
+git push
+git checkout -
+git commit -am "Bump release"
 git push
 git checkout -
 echo "Done"
